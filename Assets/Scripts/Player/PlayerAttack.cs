132342d1cfc95d990da390daed0d5d685c5c899d
lhtoan <lhtoan.13@gmail.com>
2025-10-18 14:47:34 +0700

    player attack 1

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private Animator anim;
+    private PlayerController playerController;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerController.canAttack())
+        {
+            Attack();
+        }
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<Projecttile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,137 +1,3 @@
-// using UnityEngine;
-
-// public class PlayerController : MonoBehaviour
-// {
-//     [SerializeField] private float moveSpeed = 5f;
-//     [SerializeField] private float jumpForce = 15f;
-//     [SerializeField] private LayerMask groundLayer;
-//     [SerializeField] private Transform groundCheck;
-//     private bool isGrounded;
-//     private Rigidbody2D rb;
-
-//     private void Awake()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//     }
-//     void Start()
-//     {
-
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         HandleMovement();
-//         HandleJump();
-//     }
-
-//     private void HandleMovement()
-//     {
-//         float moveInput = Input.GetAxis("Horizontal");
-//         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-//         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
-//         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
-//     }
-
-//     private void HandleJump()
-//     {
-//         if (Input.GetButton("Jump") && isGrounded)
-//         {
-//             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-//         }
-//         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-//     }
-// }
-
-// using UnityEngine;
-
-// public class PlayerController : MonoBehaviour
-// {
-//     [Header("Movement")]
-//     [SerializeField] private float moveSpeed = 5f;
-
-//     [Header("Jump")]
-//     [SerializeField] private float jumpForce = 10f;
-//     [SerializeField] private int maxJumps = 1; // số lần nhảy tối đa (1 = nhảy thường, 2 = double jump)
-//     [SerializeField] private LayerMask groundLayer;
-//     [SerializeField] private Transform groundCheck;
-//     private int jumpCount;
-//     private bool isGrounded;
-
-//     [Header("Dash")]
-//     [SerializeField] private float dashForce = 20f;
-//     [SerializeField] private float dashDuration = 0.2f;
-//     [SerializeField] private float dashCooldown = 1f;
-//     private bool isDashing;
-//     private bool canDash = true;
-
-//     private Rigidbody2D rb;
-
-//     private void Awake()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//     }
-
-//     void Update()
-//     {
-//         HandleMovement();
-
-//         if (Input.GetButtonDown("Jump"))
-//         {
-//             HandleJump();
-//         }
-
-//         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) // Nhấn Shift để Dash
-//         {
-//             StartCoroutine(Dash());
-//         }
-//     }
-
-//     private void HandleMovement()
-//     {
-//         if (isDashing) return; // khi đang dash thì không điều khiển
-
-//         float moveInput = Input.GetAxis("Horizontal");
-//         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-//         if (moveInput > 0) transform.localScale = new Vector3(1, 1, 1);
-//         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
-
-//         // Check ground
-//         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-//         if (isGrounded) jumpCount = 0; // reset khi chạm đất
-//     }
-
-//     private void HandleJump()
-//     {
-//         if (jumpCount < maxJumps)
-//         {
-//             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-//             jumpCount++;
-//         }
-//     }
-
-//     private System.Collections.IEnumerator Dash()
-//     {
-//         isDashing = true;
-//         canDash = false;
-
-//         float originalGravity = rb.gravityScale;
-//         rb.gravityScale = 0; // khi dash thì không rơi
-
-//         float dashDirection = transform.localScale.x; // hướng theo facing
-//         rb.linearVelocity = new Vector2(dashDirection * dashForce, 0f);
-
-//         yield return new WaitForSeconds(dashDuration);
-
-//         rb.gravityScale = originalGravity;
-//         isDashing = false;
-
-//         yield return new WaitForSeconds(dashCooldown);
-//         canDash = true;
-//     }
-// }
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -159,7 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         HandleMovement();
         HandleJump();
-        UpdateAnimation();
+        
     }
 
     private void HandleMovement()
@@ -190,16 +56,14 @@ public class PlayerController : MonoBehaviour
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
-    }
-
-   
-
-    private void UpdateAnimation()
-    {
-        
         bool isJumping = !isGrounded;
-
-        
         animator.SetBool("isJump", isJumping);
+
     }
+
+    public bool canAttack()
+    {
+        return true;
+    }
+
 }
