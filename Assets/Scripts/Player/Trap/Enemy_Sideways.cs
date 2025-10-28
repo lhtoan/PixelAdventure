ef743b98b275c9f0f797058033f88e574619b9a5
lhtoan <lhtoan.13@gmail.com>
2025-10-28 18:17:21 +0700

    update map

--- Assets/Scripts/Player/Trap/Enemy_Sideways.cs
+++ Assets/Scripts/Player/Trap/Enemy_Sideways.cs
@@ -2,48 +2,91 @@ using UnityEngine;
 
 public class Enemy_Sideways : MonoBehaviour
 {
-    [SerializeField] private float movementDistance;
-    [SerializeField] private float speed;
-    [SerializeField] private float damage = 1f;
-    private bool movingLeft;
-    private float leftEdge;
-    private float rightEdge;
+    // [SerializeField] private float movementDistance;
+    // [SerializeField] private float speed;
+    // [SerializeField] private float damage = 1f;
+    // private bool movingLeft;
+    // private float leftEdge;
+    // private float rightEdge;
 
-    private void Awake()
+    // private void Awake()
+    // {
+    //     leftEdge = transform.position.x - movementDistance;
+    //     rightEdge = transform.position.x + movementDistance;
+    // }
+
+    // private void Update()
+    // {
+    //     if (movingLeft)
+    //     {
+    //         if (transform.position.x > leftEdge)
+    //         {
+    //             transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+    //         }
+    //         else
+    //             movingLeft = false;
+    //     }
+    //     else
+    //     {
+    //         if (transform.position.x < rightEdge)
+    //         {
+    //             transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+    //         }
+    //         else
+    //             movingLeft = true;
+    //     }
+    // }
+
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.tag == "Player")
+    //     {
+    //         collision.GetComponent<Health>().TakeDamage(damage);
+    //     }
+    // }
+
+    [Header("Platform Movement Settings")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    private Vector2 targetPosition;
+
+    [Header("Damage Settings")]
+    [SerializeField] private float damage = 1f; // Lượng sát thương gây ra khi chạm vào
+
+    private void Start()
     {
-        leftEdge = transform.position.x - movementDistance;
-        rightEdge = transform.position.x + movementDistance;
+        targetPosition = startPoint.position;
     }
 
     private void Update()
     {
-        if (movingLeft)
+        if (Vector2.Distance(transform.position, startPoint.position) < 0.1f)
         {
-            if (transform.position.x > leftEdge)
-            {
-                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
-            }
-            else
-                movingLeft = false;
+            targetPosition = endPoint.position;
         }
-        else
+
+        if (Vector2.Distance(transform.position, endPoint.position) < 0.1f)
         {
-            if (transform.position.x < rightEdge)
-            {
-                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-            }
-            else
-                movingLeft = true;
+            targetPosition = startPoint.position;
         }
+
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            targetPosition,
+            moveSpeed * Time.deltaTime
+        );
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-            collision.GetComponent<Health>().TakeDamage(damage);
+            Health playerHealth = collision.GetComponent<Health>();
+            if (playerHealth != null)
+                playerHealth.TakeDamage(damage);
         }
     }
 
-   
+
 }
